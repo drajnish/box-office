70457de7cae1f414b994b36a8238b82c35f67e8d
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2021-12-10 11:09:34 +0530

    edit search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,11 @@ import { apiGet } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  console.log(results);
+
+  const isShowsSearch = searchOption === 'shows';
 
   const onInputChange = ev => {
     setInput(ev.target.value);
@@ -13,9 +18,16 @@ const Home = () => {
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=girls
 
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
+
+    // we refactor the below code
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    //   .then(response => response.json())
+    //   .then(result => {
+    //     setResults(result);
+    //   });
   };
 
   const onKeyDown = ev => {
@@ -24,30 +36,73 @@ const Home = () => {
     }
   };
 
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  console.log(searchOption);
+
   const renderResults = () => {
     if (results && results.length === 0) {
       return <div>Not Found</div>;
     }
 
     if (results && results.length > 0) {
-      return (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
-        </div>
-      );
+      console.log(results[0].show);
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
     }
+
+    // if (results && results.length > 0) {
+    //   return (
+    //     <div>
+    //       {results.map(item => (
+    //         <div key={item.show.id}>{item.show.name}</div>
+    //       ))}
+    //     </div>
+    //   );
+    // }
+
+    return null;
   };
 
   return (
     <MainPageLayout>
       <input
         type="text"
+        placeholder="Search for something"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
+
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
